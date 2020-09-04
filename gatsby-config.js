d30a30c5aea9a3fb74dcf23fa39d4e5731f03743
jithendraathipatla <37688287+jithendraathipatla@users.noreply.github.com>
2020-09-04 21:29:56 +0530

    making second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,17 +30,28 @@ module.exports = {
     `gatsby-transformer-json`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `orange`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
+    `gatsby-plugin-catch-links`,
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `PSI`,
+        short_name: `PSI`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-offline',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
